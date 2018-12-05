96373534505cf13ccaf4fa4143a3d7bf8332cc73
zfu <zgangfu@gmail.com>
2018-12-05 11:30:29 -0500

    Commit the change

--- 11_Fb/30-Substring with Concatenation of All Words.java
+++ 11_Fb/30-Substring with Concatenation of All Words.java
@@ -8,31 +8,26 @@ You should return the indices: [0,9].
 (order does not matter).
  */
 
-
 /**
- * 该题考察的是 Sliding Window.
- * 基本解法还是在 Sliding Window Template 的基础上面进行修改。
+ * 该题考察的是 Sliding Window. 基本解法还是在 Sliding Window Template 的基础上面进行修改。
  * 介于难度原因，相对改动较大，但是主要的思想与编码框架仍然是相同的，详见以下分析。
  * 
- * 因为words字符串数组中的字符有可能重复。因此需要使用HashMap - map保存单词以及单词出现的次数。
- *  1. 将字符串数组中的字符串存放在HashMap中，key为单词，value为单词出现的次数
- *  2. 由于单词的长度都是相同的，所以外循环的次数为单词的长度，内循环的次数为字符串s中以单词长度划分之后的次数.
- *  循环条件中，r指向s中要判断的单词的第一个字符，r每次增加一个单词的长度，r最多只能到s中最后一个单词的第一个字符的位置。
- *  3. 在内循环时,借助了一个HashMap - curr来保存窗口中的已经匹配的单词及其出现的次数，
- *  如果当前的单词在 map 中，则每当给该curr中添加一个单词时，会判断其出现的次数是否超过了hm中该单词的出现次数，
- *  如果没超过，则表示匹配单词个数要增加count++, 如果超过则需要缩小窗口。
- *  同样，由于左窗口 begin 的那个单词并不一定与当前单词相等，因此也需要从left开始一个循环找到那个次数超了的单词，
- *  同时还需要将循环中的单词的次数在 curr 中减少。直到 map 中该单词的次数与 curr 中该单词的次数相等。
- *  之后需要判断curr中所有的单词个数和count是否与hm中所有的单词的个数和是否相等。
- *  如果相等，表示从 begin 到 r 的子串是words中字符串的一个连接，此时需要加入结果集，同时 curr 和 begin 需要改变。
- *  如果在内循环中，突然有个单词不在 map 中，则表示从 begin 到 r 的串都不符合条件，
- *  则 begin = r + 单词长度，同时清空curr和count.
+ * 因为words字符串数组中的字符有可能重复。因此需要使用HashMap - map保存单词以及单词出现的次数。 1.
+ * 将字符串数组中的字符串存放在HashMap中，key为单词，value为单词出现的次数 2.
+ * 由于单词的长度都是相同的，所以外循环的次数为单词的长度，内循环的次数为字符串s中以单词长度划分之后的次数.
+ * 循环条件中，r指向s中要判断的单词的第一个字符，r每次增加一个单词的长度，r最多只能到s中最后一个单词的第一个字符的位置。 3.
+ * 在内循环时,借助了一个HashMap - curr来保存窗口中的已经匹配的单词及其出现的次数， 如果当前的单词在 map
+ * 中，则每当给该curr中添加一个单词时，会判断其出现的次数是否超过了hm中该单词的出现次数， 如果没超过，则表示匹配单词个数要增加count++,
+ * 如果超过则需要缩小窗口。 同样，由于左窗口 begin 的那个单词并不一定与当前单词相等，因此也需要从left开始一个循环找到那个次数超了的单词，
+ * 同时还需要将循环中的单词的次数在 curr 中减少。直到 map 中该单词的次数与 curr 中该单词的次数相等。
+ * 之后需要判断curr中所有的单词个数和count是否与hm中所有的单词的个数和是否相等。 如果相等，表示从 begin 到 r
+ * 的子串是words中字符串的一个连接，此时需要加入结果集，同时 curr 和 begin 需要改变。 如果在内循环中，突然有个单词不在 map
+ * 中，则表示从 begin 到 r 的串都不符合条件， 则 begin = r + 单词长度，同时清空curr和count.
  */
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> rst = new ArrayList<>();
-        if (s == null || words.length == 0
-                || s.length() < words.length * words[0].length()) {
+        if (s == null || words.length == 0 || s.length() < words.length * words[0].length()) {
             return rst;
         }
 
@@ -95,81 +90,86 @@ class Solution {
     }
 }
 
-/* 
-Hash map + two pointers
-i iterates string s, build unvisited = new HashMap<>(map) for each i
-and try to clear unvisited words, add to result when unvisited is empty
-*/
+/*
+ * Hash map + two pointers i iterates string s, build unvisited = new
+ * HashMap<>(map) for each i and try to clear unvisited words, add to result
+ * when unvisited is empty
+ */
 
-
-//Method 1: Two map, copy to unvisited map for each possible index i, verify and add to result
+// Method 1: Two map, copy to unvisited map for each possible index i, verify
+// and add to result
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
-        if (s.length() == 0 || words.length==0 ) return result;
+        if (s.length() == 0 || words.length == 0)
+            return result;
         int len = words[0].length();
         int n = words.length;
-        
+
         Map<String, Integer> map = new HashMap<>();
-        for (String word:words) map.put(word, map.containsKey(word)? map.get(word)+1:1);
-        
-        for (int i=0; i<s.length() - n * len + 1; i++){ //all possible indices
+        for (String word : words)
+            map.put(word, map.containsKey(word) ? map.get(word) + 1 : 1);
+
+        for (int i = 0; i < s.length() - n * len + 1; i++) { // all possible indices
             Map<String, Integer> unvisited = new HashMap<>(map);
-            int match = 0; //number of words matched
-            for (int j=0; j<words.length; j++){
-                String word = s.substring(i+j*len, i+(j+1)*len);
-                if (!unvisited.containsKey(word) || unvisited.get(word) < 1){
+            int match = 0; // number of words matched
+            for (int j = 0; j < words.length; j++) {
+                String word = s.substring(i + j * len, i + (j + 1) * len);
+                if (!unvisited.containsKey(word) || unvisited.get(word) < 1) {
                     break;
                 }
-                unvisited.put(word, unvisited.get(word)-1); //one more word matched
+                unvisited.put(word, unvisited.get(word) - 1); // one more word matched
                 match++;
             }
-            if (match == n) result.add(i);
+            if (match == n)
+                result.add(i);
         }
-        
+
         return result;
     }
 }
 
-
-//Method 2: Sliding window
+// Method 2: Sliding window
 class Solution {
-    //[## idea ##]
+    // [## idea ##]
     // Sliding Window
-    
+
     // ask interviewer if words is empty, should I return empty list
     // words list contains duplicated word?
-    
+
     public List<Integer> findSubstring(String S, String[] L) {
         List<Integer> res = new LinkedList<>();
-        //input validation
-        if (L.length == 0 || S.length() < L.length * L[0].length())   return res;
-        
+        // input validation
+        if (L.length == 0 || S.length() < L.length * L[0].length())
+            return res;
+
         int N = S.length(), M = L.length, K = L[0].length();
         Map<String, Integer> map = new HashMap<>(), curMap = new HashMap<>();
-        
+
         for (String s : L) {
-            map.put(s, map.getOrDefault(s,0)+1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         String str = null, tmp = null;
-        //traverse all the posibilities
+        // traverse all the posibilities
         for (int i = 0; i < K; i++) {
-            int count = 0;  // remark: reset count 
-            //l is the start point, r+k is the end point
+            int count = 0; // remark: reset count
+            // l is the start point, r+k is the end point
             for (int l = i, r = i; r + K <= N; r += K) {
                 str = S.substring(r, r + K);
                 if (map.containsKey(str)) {
-                    curMap.put(str, curMap.getOrDefault(str,0) + 1);
-                    
-                    if(curMap.get(str) <= map.get(str)) count++;
-                    //move l
+                    curMap.put(str, curMap.getOrDefault(str, 0) + 1);
+
+                    if (curMap.get(str) <= map.get(str))
+                        count++;
+                    // move l
                     while (curMap.get(str) > map.get(str)) {
                         tmp = S.substring(l, l + K);
                         curMap.put(tmp, curMap.get(tmp) - 1);
                         l += K;
-                        if (curMap.get(tmp) < map.get(tmp)) count--;
+                        if (curMap.get(tmp) < map.get(tmp))
+                            count--;
                     }
-                    
+
                     if (count == M) {
                         res.add(l);
                         tmp = S.substring(l, l + K);
@@ -177,7 +177,7 @@ class Solution {
                         l += K;
                         count--;
                     }
-                }else {
+                } else {
                     curMap.clear();
                     count = 0;
                     l = r + K;
@@ -190,37 +190,39 @@ class Solution {
 }
 
 // https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/175458/Beats-80-Sliding-Window-Java-with-Explanations
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
 
-public List<Integer> findSubstring(String s, String[] words) {
-        
-    if (s == null || s.length() == 0 || words == null || words.length == 0) // Corner cases.
-        return new LinkedList<>();
-    int wordLen = words[0].length(), n = s.length(), maxWindowSize = words.length * wordLen; 
-    List<Integer> result = new LinkedList<>(); // Result list.
-    Map<String, Integer> wordDict = new HashMap<>(); // key: word in words; value: frequency of a word.       
-    for (String word : words) {
-        wordDict.put(word, wordDict.getOrDefault(word, 0) + 1);
-    }
-    Map<String, Integer> windowDict; // key: word in the current window; value: frequency of a word      
-    
-    for (int round = 0; round < wordLen; round++) { // wordLen rounds could cover all possible starting indices if we check wordLen characters by wordLen characters.
-        windowDict = new HashMap<>();
-        for (int i = round, j = round; j + wordLen <= n; j += wordLen) {
-            String word = s.substring(j, j + wordLen);
-            if (wordDict.containsKey(word)) {
-                windowDict.put(word, windowDict.getOrDefault(word, 0) + 1);   
-            }               
-            if ((j - i + 1) > maxWindowSize) { // Current window is over-sized. 
-                word = s.substring(i, i + wordLen);
-                if (wordDict.containsKey(word)) {
-                    windowDict.put(word, windowDict.get(word) - 1);
-                }
-                i += wordLen;
-            }               
-            if (wordDict.equals(windowDict))
-                result.add(i);
+        if (s == null || s.length() == 0 || words == null || words.length == 0) // Corner cases.
+            return new LinkedList<>();
+        int wordLen = words[0].length(), n = s.length(), maxWindowSize = words.length * wordLen;
+        List<Integer> result = new LinkedList<>(); // Result list.
+        Map<String, Integer> wordDict = new HashMap<>(); // key: word in words; value: frequency of a word.
+        for (String word : words) {
+            wordDict.put(word, wordDict.getOrDefault(word, 0) + 1);
         }
+        Map<String, Integer> windowDict; // key: word in the current window; value: frequency of a word
+
+        for (int round = 0; round < wordLen; round++) { // wordLen rounds could cover all possible starting indices if
+                                                        // we check wordLen characters by wordLen characters.
+            windowDict = new HashMap<>();
+            for (int i = round, j = round; j + wordLen <= n; j += wordLen) {
+                String word = s.substring(j, j + wordLen);
+                if (wordDict.containsKey(word)) {
+                    windowDict.put(word, windowDict.getOrDefault(word, 0) + 1);
+                }
+                if ((j - i + 1) > maxWindowSize) { // Current window is over-sized.
+                    word = s.substring(i, i + wordLen);
+                    if (wordDict.containsKey(word)) {
+                        windowDict.put(word, windowDict.get(word) - 1);
+                    }
+                    i += wordLen;
+                }
+                if (wordDict.equals(windowDict))
+                    result.add(i);
+            }
+        }
+
+        return result;
     }
-    
-    return result;
 }
